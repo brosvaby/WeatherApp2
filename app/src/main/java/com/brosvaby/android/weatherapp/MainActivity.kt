3e0016d563c356c8fa06d62401919577ccc33a82
brosvaby <brosvaby@naver.com>
2022-12-27 01:09:34 +0900

    completed
    completed

--- app/src/main/java/com/brosvaby/android/weatherapp/MainActivity.kt
+++ app/src/main/java/com/brosvaby/android/weatherapp/MainActivity.kt
@@ -6,28 +6,31 @@ import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.brosvaby.android.weatherapp.adapter.CityAdapter
 import com.brosvaby.android.weatherapp.api.ApiClient
+import com.brosvaby.android.weatherapp.databinding.ActivityMainBinding
 import com.brosvaby.android.weatherapp.viewmodel.CityViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var cityAdapter: CityAdapter
     private lateinit var viewModel: CityViewModel
     var loadingDialog: Dialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val rvCity = findViewById<RecyclerView>(R.id.rv_city)
         viewModel = ViewModelProvider(this)[CityViewModel::class.java]
         viewModel.client = ApiClient(this)
         loadingDialog = LoadingDialog(this)
+        cityAdapter = CityAdapter(this@MainActivity)
 
-        rvCity.layoutManager = LinearLayoutManager(this)
-        cityAdapter = CityAdapter(MainActivity@ this)
-        rvCity.adapter = cityAdapter
+        binding.rvCity.run {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = cityAdapter
+        }
 
         var resultObserver = Observer<ArrayList<Any>> { result ->
             cityAdapter.addAll(result)
@@ -40,10 +43,10 @@ class MainActivity : AppCompatActivity() {
         viewModel.items.observe(this, resultObserver)
         viewModel.isLoading.observe(this, isLoadingObserver)
         viewModel.states.observe(this, Observer { pState ->
-            when(pState) {
+            when (pState) {
                 is CityViewModel.CallWeather -> {
-                    if(pState.city == "London") viewModel.getCityList("Seoul")
-                    if(pState.city == "Seoul") viewModel.getCityList("Chicago")
+                    if (pState.city == "London") viewModel.getCityList("Seoul")
+                    if (pState.city == "Seoul") viewModel.getCityList("Chicago")
                 }
             }
         })
